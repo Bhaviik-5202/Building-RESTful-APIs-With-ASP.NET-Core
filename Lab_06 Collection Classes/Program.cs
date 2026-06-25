6025159360ee911f5cdc9feee6425accd7b767fa
BhaviikCode-5202 <er.bhavik5202@gmail.com>
2026-06-25 21:51:48 +0530

    Lab_06 Collection Classes Start

---
+++ Lab_06 Collection Classes/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Lab_06_Collection_Classes
+{
+    internal class Program
+    {
+        static List<Student> stu = new List<Student>();
+        static void Main(string[] args)
+        {
+            /* 1. Develop a menu-driven console application to manage student records using List. 
+             * Implement Add, Display, Search, Update, and Delete functionalities.
+             */
+            int choice = 0;
+            do
+            {
+                Console.WriteLine("1. Add Student");
+                Console.WriteLine("2. Display Students");
+                Console.WriteLine("3. Search Student");
+                Console.WriteLine("4. Update Student");
+                Console.WriteLine("5. Delete Student");
+                Console.WriteLine("6. Exit");
+                Console.Write("Enter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        AddStudent();
+                        break;
+                    case 2:
+                        DisplayStudent();
+                        break;
+                    case 3:
+                        SearchStudent();
+                        break;
+                    case 4:
+                        UpdateStudent();
+                        break;
+                    case 5:
+                        DeleteStudent();
+                        break;
+                    case 6:
+                        Console.WriteLine("Exit.");
+                        return;
+                    default:
+                        Console.WriteLine("Plese, Enter Valid Choise");
+                        break;
+                }
+            } while (choice != 6);
+        }
+        static void AddStudent()
+        {
+            Console.WriteLine("Enter Student Id : ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Student Name : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Student Age :");
+            int age = Convert.ToInt32(Console.ReadLine());
+            stu.Add(new Student(id, name, age));
+
+            Console.WriteLine("Student Add Successfully");
+        }
+
+
+        static void DisplayStudent()
+        {
+            if (stu.Count == 0)
+            {
+                Console.WriteLine("Student List Empty.");
+            }
+            foreach (var student in stu)
+            {
+                Console.WriteLine($"ID: {student.id}, Name: {student.name}, Age: {student.age}");
+            }
+        }
+
+        static void SearchStudent()
+        {
+            Console.WriteLine("Enter Student Id To Search : ");
+            int id = Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void UpdateStudent()
+        {
+
+        }
+
+        static void DeleteStudent()
+        {
+
+        }
+    }
+}
---
+++ Lab_06 Collection Classes/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_06_Collection_Classes
+{
+    internal class Student
+    {
+        public int id;
+        public string name;
+        public int age;
+
+        public Student(int id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
