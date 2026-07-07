9f02d95a952966dc2dd6f731a0590ea56e486133
BhaviikCode-5202 <er.bhavik5202@gmail.com>
2026-07-07 23:50:17 +0530

    Lab_08 Create Web API With Migration

---
+++ API_With_Status_Code/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace API_With_Status_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
